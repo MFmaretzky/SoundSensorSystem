8ea962787f881eac770c18f6946b77c9cae041b5
Marek <161449890+MFmaretzky@users.noreply.github.com>
2024-09-11 23:13:48 +0200

    Collecting sampled improved

--- Program.cs
+++ Program.cs
@@ -4,62 +4,59 @@ using UnitsNet;
 using Newtonsoft.Json;
 using Microsoft.Azure.Devices.Client;
 using System.Text;
+using MathNet.Numerics.IntegralTransforms;
+using System.Numerics;
 
 namespace Scsys
 {
   class Program
   {
     static int i = 0;
+
     public class Readout
     {
-      public short RawValue { get; set; }
       public double Voltage { get; set; }
       public double DbValue { get; set; }
       public string? Time { get; set; }
       public string? Date { get; set; }
+      public List<double>? Frequencies { get; set; }
     }
 
-    // Change the return type of Main to Task
-    static async Task Main(string[] args)
+    static async Task Main()
     {
-      /* Initialize connection to IoT hub */
+        /* Initialize connection to IoT hub */
       DeviceClient deviceClient;
-      string iotHubUri = "IoTSoundSensorHub.azure-devices.net";
-      string deviceKey = "hTMcf2QVF7cncI+d5e+J6rsSi/W9gibdVAIoTPc0MVc=";
+      string iotHubUri = "SoundHub.azure-devices.net";
+      string deviceKey = "V1ARX+ydYUrQlDcGJbUjekS90Z97l16aBAIoTNntwL0=";
       deviceClient = DeviceClient.Create(iotHubUri, new DeviceAuthenticationWithRegistrySymmetricKey("morkiraspi", deviceKey), TransportType.Mqtt);
 
-      /* Initialize I2C settings */
+        /* Initialize I2C settings */
       int busId = 1;
       int deviceAddress = 0x49;
       var i2cSettings = new I2cConnectionSettings(busId, deviceAddress);
       var device = I2cDevice.Create(i2cSettings);
 
-      /* Initialize ADS1115 */
+        /* Initialize ADS1115 */
       Ads1115 adc = new Ads1115(device, InputMultiplexer.AIN0, MeasuringRange.FS6144);
       Console.WriteLine("ADS1115 initialized successfully.");
 
-      /* Initialise dictionary of lists to hold samples */
+        /* Initialise dictionary of lists to hold samples */
       Dictionary<InputMultiplexer, List<Readout>> samples = new() {
-        { InputMultiplexer.AIN0, new List<Readout>() },
-        { InputMultiplexer.AIN1, new List<Readout>() },
-        { InputMultiplexer.AIN2, new List<Readout>() },
         { InputMultiplexer.AIN3, new List<Readout>() }
       };
-
 
       while (true)
       {
         Console.WriteLine($"Readout no.{++i}");
         CollectSamples(adc, samples);
 
-        /* Convert the dictionary to JSON and print */
+          /* Convert the dictionary to JSON and print */
         string serializedData = JsonConvert.SerializeObject(samples, Formatting.Indented);
         var serializedEncodedData = new Message(Encoding.ASCII.GetBytes(serializedData));
+        await deviceClient.SendEventAsync(serializedEncodedData);
+
         Console.WriteLine($"{serializedData}");
-
-        await deviceClient.SendEventAsync(serializedEncodedData); // Await the async call
-
-        Thread.Sleep(2000);
+        Thread.Sleep(10);
       }
     }
 
@@ -75,33 +72,53 @@ namespace Scsys
       foreach (var channel in samples.Keys.ToList())
       {
         samples[channel].Clear();
-        for (int i = 0; i < 2; i++)
+        List<double> voltages = new List<double>();
+
+        for (int i = 0; i < 10; i++)
         {
           ElectricPotential voltage = ReadAds1115Channel(adc, channel);
           short rawValue = adc.ReadRaw();
           double dbValue = voltage.Volts * 50;
+          voltages.Add(voltage.Volts);
 
-          /* Create a Readout object and add it to the list */
           Readout readout = new Readout
           {
-            RawValue = rawValue,
             Voltage = voltage.Volts,
             DbValue = dbValue,
-            Time = DateTime.Now.ToString("HH:mm:ss"),
-            Date = DateTime.Now.ToString("yyyy-MM-dd")
+            Time = DateTime.Now.ToString("HH:mm:ss.f"),
+            Date = DateTime.Now.ToString("yyyy-MM-dd"),
+            Frequencies = null
           };
           samples[channel].Add(readout);
-
-          Thread.Sleep(10);
+          Thread.Sleep(2);
         }
-      }
 
-      foreach (var channel in samples.Keys)
-      {
-        var channelSamples = samples[channel];
-        ElectricPotential average = ElectricPotential.FromVolts(channelSamples.Average(v => v.Voltage));
-        ElectricPotential max = channelSamples.Max(v => ElectricPotential.FromVolts(v.Voltage));
+        //var fftResult = PerformFFT(voltages.ToArray());
+
+        //foreach (var readout in samples[channel])
+        //{
+          //readout.Frequencies = fftResult.ToList();
+        //}
       }
+    }
+
+    static double[] PerformFFT(double[] data)
+    {
+      int n = data.Length;
+      Complex[] fftData = new Complex[n];
+
+      for (int i = 0; i < n; i++)
+      {
+        fftData[i] = new Complex(data[i], 0);
+      }
+      Fourier.Forward(fftData, FourierOptions.Matlab);
+
+      double[] frequencies = new double[n];
+      for (int i = 0; i < n; i++)
+      {
+        frequencies[i] = fftData[i].Magnitude;
+      }
+      return frequencies;
     }
   }
 }
